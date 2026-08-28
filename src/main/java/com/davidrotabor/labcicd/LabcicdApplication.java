5e2a35bc465dd797bc195ed8ba629fc252cb94fb
David Rodríguez <davidrotabor@gmail.com>
2026-08-27 20:57:08 -0500

    prueba

--- src/main/java/com/davidrotabor/labcicd/LabcicdApplication.java
+++ src/main/java/com/davidrotabor/labcicd/LabcicdApplication.java
@@ -2,7 +2,7 @@ package com.davidrotabor.labcicd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//
 @SpringBootApplication
 public class LabcicdApplication {
 
